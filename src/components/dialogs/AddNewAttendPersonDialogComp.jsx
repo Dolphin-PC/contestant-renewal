56e6b9b7debc2f6006bd81c588f6bf3b67dacb8f
Dolphin-PC <pcx474@gmail.com>
2021-02-09 11:33:56 +0900

    error fix 출석일정Dialog, TransferList Splict code

--- src/components/dialogs/AddNewAttendPersonDialogComp.jsx
+++ src/components/dialogs/AddNewAttendPersonDialogComp.jsx
@@ -27,6 +27,7 @@ import {
   SaveAttends,
   SavePreset,
 } from "actions/dbActions";
+import TransferList from "components/transfer/TransferList";
 
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -47,7 +48,7 @@ const AddNewAttendPersonDialogComp = ({
   const [right, setRight] = React.useState([]);
 
   const presetNameRef = useRef();
-  const [currentPresets, setCurrentPresets] = useState(null);
+  const [currentPresets, setCurrentPresets] = useState(undefined);
 
   const handleCreateAttend = useCallback(() => {
     dispatch(SaveAttends(currentAttend, right));
@@ -66,7 +67,7 @@ const AddNewAttendPersonDialogComp = ({
 
   const handleSelectPreset = useCallback((e) => {
     if (e.target.value === "") {
-      setCurrentPresets(null);
+      setCurrentPresets(undefined);
     } else {
       setCurrentPresets(e.target.value);
       presetNameRef.current.value = "";
@@ -115,168 +116,12 @@ const AddNewAttendPersonDialogComp = ({
   }, [open]);
 
   useEffect(() => {
-    if (currentPresets !== null && currentPresets.preset != null) {
+    if (currentPresets !== undefined && currentPresets.preset !== undefined) {
       setRight(Object.values(currentPresets.preset));
     } else {
       setRight([]);
     }
   }, [currentPresets]);
-
-  const TransferList = () => {
-    const useStyles = makeStyles((theme) => ({
-      root: {
-        margin: "auto",
-      },
-      paper: {
-        width: 200,
-        height: 230,
-        overflow: "auto",
-      },
-      button: {
-        margin: theme.spacing(0.5, 0),
-      },
-    }));
-
-    function not(a, b) {
-      return a.filter((value) => b.indexOf(value) === -1);
-    }
-
-    function intersection(a, b) {
-      return a.filter((value) => b.indexOf(value) !== -1);
-    }
-    const classes = useStyles();
-    const [checked, setChecked] = useState([]);
-
-    const leftChecked = intersection(checked, left);
-    const rightChecked = intersection(checked, right);
-
-    const handleToggle = (value) => () => {
-      const currentIndex = checked.indexOf(value);
-      const newChecked = [...checked];
-
-      if (currentIndex === -1) {
-        newChecked.push(value);
-      } else {
-        newChecked.splice(currentIndex, 1);
-      }
-
-      setChecked(newChecked);
-    };
-
-    const handleAllRight = () => {
-      setRight(right.concat(left));
-      // setLeft([]);
-    };
-
-    const handleCheckedRight = () => {
-      setRight(right.concat(leftChecked));
-      // setLeft(not(left, leftChecked));
-      setChecked(not(checked, leftChecked));
-    };
-
-    const handleCheckedLeft = () => {
-      setLeft(left.concat(rightChecked));
-      setRight(not(right, rightChecked));
-      setChecked(not(checked, rightChecked));
-    };
-
-    const handleAllLeft = () => {
-      setLeft(left.concat(right));
-      setRight([]);
-    };
-
-    const customList = (items) => (
-      <Paper className={classes.paper}>
-        <List dense component="div" role="list">
-          {items.map((item, index) => {
-            return (
-              <ListItem
-                key={index}
-                role="listitem"
-                button
-                onClick={handleToggle(item)}
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    checked={checked.indexOf(item) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                  />
-                </ListItemIcon>
-                <Tooltip title={item.property}>
-                  <ListItemText id={index} primary={`${item.name}`} />
-                </Tooltip>
-              </ListItem>
-            );
-          })}
-          <ListItem />
-        </List>
-      </Paper>
-    );
-
-    return (
-      <Grid
-        container
-        spacing={2}
-        justify="center"
-        alignItems="center"
-        className={classes.root}
-      >
-        <Grid item className="Left-Attend">
-          <h5>동아리원 목록({left.length})</h5>
-          {customList(left)}
-        </Grid>
-        <Grid item className="Center-Attend-Button">
-          <Grid container direction="column" alignItems="center">
-            <Button
-              variant="outlined"
-              size="small"
-              className={classes.button}
-              onClick={handleAllRight}
-              disabled={left.length === 0}
-              aria-label="move all right"
-            >
-              ≫
-            </Button>
-            <Button
-              variant="outlined"
-              size="small"
-              className={classes.button}
-              onClick={handleCheckedRight}
-              disabled={leftChecked.length === 0}
-              aria-label="move selected right"
-            >
-              &gt;
-            </Button>
-            <Button
-              variant="outlined"
-              size="small"
-              className={classes.button}
-              onClick={handleCheckedLeft}
-              disabled={rightChecked.length === 0}
-              aria-label="move selected left"
-            >
-              &lt;
-            </Button>
-            <Button
-              variant="outlined"
-              size="small"
-              className={classes.button}
-              onClick={handleAllLeft}
-              disabled={right.length === 0}
-              aria-label="move all left"
-            >
-              ≪
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid item className="Right-Attend">
-          <h5>출석할 인원 목록({right.length})</h5>
-          {customList(right)}
-        </Grid>
-      </Grid>
-    );
-  };
 
   // * 프리셋 설정 다이얼로그
   if (isPreset) {
@@ -285,7 +130,12 @@ const AddNewAttendPersonDialogComp = ({
         <DialogTitle>출석 인원 프리셋 설정</DialogTitle>
         <Divider />
         <DialogContent>
-          <TransferList />
+          <TransferList
+            left={left}
+            right={right}
+            setLeft={setLeft}
+            setRight={setRight}
+          />
         </DialogContent>
         <DialogActions>
           <Tooltip
@@ -293,7 +143,9 @@ const AddNewAttendPersonDialogComp = ({
             placement="left"
           >
             <Select
-              value={currentPresets ? currentPresets.presetName : ""}
+              value={
+                currentPresets !== undefined ? currentPresets.presetName : ""
+              }
               onChange={handleSelectPreset}
             >
               <MenuItem value="">프리셋 생성</MenuItem>
@@ -343,18 +195,21 @@ const AddNewAttendPersonDialogComp = ({
     return (
       <Dialog open={open} onClose={handleClose} fullWidth>
         <DialogTitle>
-          <h5>
-            출석 인원 추가 &emsp;
-            {currentAttend && (
-              <small>
-                {currentAttend.scheduleTime}&ensp;{currentAttend.scheduleName}
-              </small>
-            )}
-          </h5>
+          출석 인원 추가 &emsp;
+          {currentAttend && (
+            <small>
+              {currentAttend.scheduleTime}&ensp;{currentAttend.scheduleName}
+            </small>
+          )}
         </DialogTitle>
         <Divider />
         <DialogContent>
-          <TransferList />
+          <TransferList
+            left={left}
+            right={right}
+            setLeft={setLeft}
+            setRight={setRight}
+          />
         </DialogContent>
         <DialogActions>
           <Select
---
+++ src/components/transfer/TransferList.jsx
@@ -0,0 +1,171 @@
+import {
+  Button,
+  Checkbox,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Paper,
+  Tooltip,
+} from "@material-ui/core";
+import React, { useState } from "react";
+
+const TransferList = ({ left, right, setLeft, setRight }) => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      margin: "auto",
+    },
+    paper: {
+      width: 200,
+      height: 230,
+      overflow: "auto",
+    },
+    button: {
+      margin: theme.spacing(0.5, 0),
+    },
+  }));
+
+  function not(a, b) {
+    return a.filter((value) => b.indexOf(value) === -1);
+  }
+
+  function intersection(a, b) {
+    return a.filter((value) => b.indexOf(value) !== -1);
+  }
+  const classes = useStyles();
+  const [checked, setChecked] = useState([]);
+
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const handleAllRight = () => {
+    setRight(right.concat(left));
+    // setLeft([]);
+  };
+
+  const handleCheckedRight = () => {
+    setRight(right.concat(leftChecked));
+    // setLeft(not(left, leftChecked));
+    setChecked(not(checked, leftChecked));
+  };
+
+  const handleCheckedLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked));
+    setChecked(not(checked, rightChecked));
+  };
+
+  const handleAllLeft = () => {
+    setLeft(left.concat(right));
+    setRight([]);
+  };
+
+  const customList = (items) => (
+    <Paper className={classes.paper}>
+      <List dense component="div" role="list">
+        {items.map((item, index) => {
+          return (
+            <ListItem
+              key={index}
+              role="listitem"
+              button
+              onClick={handleToggle(item)}
+            >
+              <ListItemIcon>
+                <Checkbox
+                  checked={checked.indexOf(item) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                />
+              </ListItemIcon>
+              <Tooltip title={item.property}>
+                <ListItemText id={index} primary={`${item.name}`} />
+              </Tooltip>
+            </ListItem>
+          );
+        })}
+        <ListItem />
+      </List>
+    </Paper>
+  );
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Grid item className="Left-Attend">
+        <h5>동아리원 목록({left.length})</h5>
+        {customList(left)}
+      </Grid>
+      <Grid item className="Center-Attend-Button">
+        <Grid container direction="column" alignItems="center">
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleAllRight}
+            disabled={left.length === 0}
+            aria-label="move all right"
+          >
+            ≫
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleCheckedRight}
+            disabled={leftChecked.length === 0}
+            aria-label="move selected right"
+          >
+            &gt;
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleCheckedLeft}
+            disabled={rightChecked.length === 0}
+            aria-label="move selected left"
+          >
+            &lt;
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleAllLeft}
+            disabled={right.length === 0}
+            aria-label="move all left"
+          >
+            ≪
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item className="Right-Attend">
+        <h5>출석할 인원 목록({right.length})</h5>
+        {customList(right)}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TransferList;
